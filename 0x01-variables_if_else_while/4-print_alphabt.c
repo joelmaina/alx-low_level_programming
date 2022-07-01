5c93bb6ba28403ca4765b58c7c173ce2a451d82f
joelmaina <joeljack5474@2gmail.com>
2022-07-01 02:47:28 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+char first = 'a';
+char end = '\n';
+while (first <= 'z')
+{
+if (first !=  'q' && fist != 'e')
+{
+putchar (first);
+}
+first++;
+}
+putchar (end);
+return (0);
+}
