2a3fb01d1e57d937ad93311753418959dfb5cdfe
joelmaina <joeljack5474@2gmail.com>
2022-07-01 03:45:01 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+char mwisho= 'z';
+char kikomo = '\n';
+while (mwisho >= 'a')
+{
+putchar (mwisho);
+mwisho--;
+}
+putchar (kikomo);
+return (0);
+}
