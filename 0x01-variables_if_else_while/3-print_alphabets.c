28a451c94171d0f0ed480884be54d8eac07c1128
joelmaina <joeljack5474@2gmail.com>
2022-07-01 02:06:24 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+char small = 'a';
+char big = 'A';
+char last = '\n';
+while (small <= 'z')
+{
+putchar (small);
+small++;
+}
+
+while (big <= 'Z')
+{
+putchar (big);
+big++;
+}
+putchar (last);
+return (0);
+}
