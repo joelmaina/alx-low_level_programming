b85b62ad462731db6aa849b9bc7160fc55973c84
joelmaina <joeljack5474@2gmail.com>
2022-07-01 03:01:27 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i = 0;
+for (i = 0; i <= 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
