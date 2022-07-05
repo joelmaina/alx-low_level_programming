b1942e382684bcc77737938f9eb6ef82ab854a48
joelmaina <joeljack5474@2gmail.com>
2022-07-05 08:48:13 -0400

    task 7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -56,5 +56,10 @@ int print_sign(int n);
  * Return: Always 0 (Success)
  */
 int _abs(int);
-
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
