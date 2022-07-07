6ade905c720685d6ec1653a2c51bd30ee170bf11
joelmaina <joeljack5474@2gmail.com>
2022-07-07 07:13:01 -0400

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-char retorno = '\n';
+char r = '\n';
 int i;
 for (i = 48; i < 58 ; i++)
 {
 if (i != 50 && i != 52)
 _putchar (i);
 }
-_putchar (retorno);
+_putchar (r);
 }
