46956a08563c8912c16d83e8b350438ae77efba5
joelmaina <joeljack5474@2gmail.com>
2022-07-07 04:00:31 -0400

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c);
+{
+	char c;
+	if(c = 'A')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+	return(0)
+
+}
